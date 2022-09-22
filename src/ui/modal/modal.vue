e84183225dd39599628004e4cb5a8615b739e53e
xubowen <bowen.xu@bangdao-tech.com>
2022-09-22 09:13:19 +0800

    fix: ui 新增modal

--- src/ui/modal/modal.vue
+++ src/ui/modal/modal.vue
@@ -26,68 +26,68 @@ export default {
     top: {
       type: String,
       default() {
-        return "50px";
+        return "50px"
       }
     },
     clickMaskHide: {
       type: Boolean,
       default() {
-        return true;
+        return true
       }
     }
   },
   data() {
     return {
       currentValue: this.value
-    };
+    }
   },
   watch: {
     value(n) {
-      this.currentValue = n;
+      this.currentValue = n
     }
   },
   methods: {
     close() {
-      this.currentValue = false;
-      this.$emit("input", false);
+      this.currentValue = false
+      this.$emit("input", false)
     },
     clickMask() {
       if (this.clickMaskHide) {
-        this.close();
+        this.close()
       }
     }
   }
-};
+}
 </script>
 <style lang="sass" scoped>
 .modal
   display: block
   opacity: 0
   z-index: -1
-  transition: all 0.2s linear;
-  // transform: translateY(-80px);
+  transition: all 0.2s linear
+  // transform: translateY(-80px)
   &.is-active
       opacity: 1
       z-index: 40
-      transition: all 0.2s linear;
-      // transform: translateY(0px);
+      transition: all 0.2s linear
+      // transform: translateY(0px)
       .modal-background
-        transition: all 0.2s linear;
+        transition: all 0.2s linear
         background-color: rgba(35,45,65,.7)
       .modal-card
-        transition: all 0.2s linear;
-        transform: translateY(0px);
+        transition: all 0.2s linear
+        transform: translateY(0px)
 .modal-background
   position: fixed
   top: 0
   bottom: 0
   left: 0
   right: 0
-  transition: all 0.2s linear;
+  transition: all 0.2s linear
   background-color: transparent
 .modal-card
-   transition: all 0.2s linear;
-   transform: translateY(-80px);
+   transition: all 0.2s linear
+   transform: translateY(-80px)
 .modal-card-foot
   div
     flex: 1
